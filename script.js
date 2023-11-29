1778c4f9a1661cdc69aee5da737e69cc96829321
¨Jane <smoreira11@hotmail.com>
2023-11-29 22:52:03 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//HTML element 
+const board = document.getElementById("board");
+const scoreBoard = document.getElementById("scoreBoard");
+const startButton = document.getElementById("gameOver");
+
+// Game seatting 
+const boardSize = 10;
+const gameSpeed = 100;
